5513ae6e6ba837dafe61822356063a92c76c8661
Antoine Pascual <74872926+Flashtheorie@users.noreply.github.com>
2022-03-29 18:42:37 +0200

    Ajout des consignes en commentaire

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-// Exercice 1 : Trouver les trois dernieres lettres d'un string
-function lastthree(str) {
+// Exercice 1 : 
+// Write a function that takes a string (a) and a number (n) as argument
+// Return the nth character of 'a'
+
+
+function lastthree(str, number) {
     let length = str.length;
-    let lenghtmoins = str.length - 3;
+    let lenghtmoins = str.length - number;
     console.log(str.slice(lenghtmoins, length));
 }
 
-lastthree("Couillasse de la ville");
+// Décommenter si utilisation 
+//lastthree("Couillasse de la ville", 3);
 
 
-// Exercice 2 : Retourner le type dela variable
+// Exercice 2 :
+// Write a function that takes a value as argument
+// Return the type of the value
+
+
+function whatType(str){
+    console.log(typeof(str));
+}
+// Décommenter si utilisation 
+// whatType(3)
+
+// Exercice 3 : 
+// Write a function that a string (a) as argument
+// Create an object that has a property with key 'key' and a value of a
+// Return the object
